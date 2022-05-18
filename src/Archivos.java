f0bdc29ff665108d48cd2907ef3f13e64586c1dc
Manuel <manuelalejandromartinezf@gmail.com>
2022-05-18 10:35:59 -0600

    Implementar Archivos

---
+++ src/Archivos.java
@@ -0,0 +1,24 @@
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class Archivos {
+
+	/**
+	 * Lee y evalua expresiones en un archivo
+	 * @param data nombre de archivo
+	 * @throws IOException
+	 */
+	public static GuateGraph leer(String path) throws IOException {		
+		BufferedReader reader = new BufferedReader(new FileReader(path));
+        String row;
+        GuateGraph g = new GuateGraph();
+        while ((row = reader.readLine()) != null){
+        	String[] data = row.split(" ");
+        	g.addEdge(data[0], data[1], Float.valueOf(data[2]));
+        }
+        reader.close();
+        return g;
+	}
+}
